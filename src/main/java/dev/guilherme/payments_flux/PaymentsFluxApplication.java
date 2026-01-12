93e2970912673591c0171818fb0c8f75ec2684f0
GuilhermeD9 <sgsouza.173@gmail.com>
2026-01-12 15:48:31 -0300

    Initial archives with initialzr

---
+++ src/main/java/dev/guilherme/payments_flux/PaymentsFluxApplication.java
@@ -0,0 +1,13 @@
+package dev.guilherme.payments_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsFluxApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PaymentsFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/guilherme/payments_flux/PaymentsFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilherme.payments_flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
