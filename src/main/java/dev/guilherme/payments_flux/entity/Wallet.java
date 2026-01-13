14c6e576563c2d062a58e3ffd283fcf846d6a815
GuilhermeD9 <sgsouza.173@gmail.com>
2026-01-13 15:34:09 -0300

    feat: New entity Wallet

---
+++ src/main/java/dev/guilherme/payments_flux/entity/Wallet.java
@@ -0,0 +1,30 @@
+package dev.guilherme.payments_flux.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "tb_wallet")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String cpfCnpj;
+
+    @Column(unique = true)
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private BigDecimal balance;
+}
